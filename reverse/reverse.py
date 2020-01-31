e23e104db532027fc42ff49f8a4499b6efb54ac8
Gustavo Isturiz <isturizgustavo@gmail.com>
2020-01-31 16:08:39 -0500

    sprint MVP

--- reverse/reverse.py
+++ reverse/reverse.py
@@ -44,4 +44,14 @@ class LinkedList:
 
   def reverse_list(self):
     # TO BE COMPLETED
-    pass
+    previous = None
+
+    current = self.head
+
+    while current:
+      next_inline = current.next_node
+      current.next_node = previous
+      previous = current
+      current = next_inline
+      
+    self.head = previous
